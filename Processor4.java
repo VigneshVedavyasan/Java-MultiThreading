c59d484cf3f6a17924c8c84bfe483d0f519ec70b
Vignesh V <tovigu@gmail.com>
2016-01-22 21:25:14 +0530

    Create Processor4.java
    Producer Consumer problem

---
+++ Processor4.java
@@ -0,0 +1,43 @@
+package JavaThreads;
+
+import java.util.LinkedList;
+import java.util.Random;
+
+public class Processor4 {
+	private LinkedList<Integer> list = new LinkedList<Integer>();
+	private final int LIMIT = 10;
+	private Object lock = new Object();
+	
+	public void producer() throws InterruptedException{
+		int value = 0;
+
+		while (true){
+			synchronized(lock){
+				
+				while(list.size() == LIMIT){
+					lock.wait();
+				}
+				list.add(value++);
+				lock.notify();
+			}
+
+		}
+	}
+	
+	public void consumer() throws InterruptedException{
+		Random random = new Random();
+		while(true){
+			synchronized(lock){
+				while(list.size() == 0){
+					lock.wait();
+				}
+				System.out.println("List size is: " +list.size());
+				int value = list.removeFirst();
+				System.out.println("; value is: "+value);
+				lock.notify();
+			}
+			Thread.sleep(random.nextInt(1000));
+		}
+	}
+
+}
